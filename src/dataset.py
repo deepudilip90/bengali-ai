db6917366a6b3c12ef7b6d96c66d9e6f38261ad8
Deepu Dilip <deepud90@vt.edu>
2020-03-03 23:22:49 +0530

    Intermediate commit - needs debugging

--- src/dataset.py
+++ src/dataset.py
@@ -3,6 +3,7 @@ import numpy as np
 import joblib
 from PIL import Image
 import albumentations
+import torch
 
 class BengaliDatasetTrain:
 
@@ -12,15 +13,15 @@ class BengaliDatasetTrain:
         df = df[['image_id', 'grapheme_root', 'vowel_diacritic', 'consonant_diacritic', 'kfold']]
 
         df = df[df.kfold.isin(folds)].reset_index(drop=True)
-        self.image_ids = df.imange_id.values
-        self.grapheme_root = df.imange_id.grapheme_root.values
-        self.vowel_diacritic = df.imange_id.vowel_diacritic.values
-        self.consonant_diacritic = df.imange_id.consonant_diacritic.values
+        self.image_ids = df.image_id.values
+        self.grapheme_root = df.grapheme_root.values
+        self.vowel_diacritic = df.vowel_diacritic.values
+        self.consonant_diacritic = df.consonant_diacritic.values
 
         if len(folds) == 1:
        
             self.aug  = albumentations.Compose([
-                albumentations.Resize(img_height, img_width, always_apply=True)
+                albumentations.Resize(img_height, img_width, always_apply=True),
                 albumentations.Normalize(mean, std, always_apply=True)
 
             ])
@@ -30,7 +31,7 @@ class BengaliDatasetTrain:
                 albumentations.ShiftScaleRotate(shift_limit=0.0625, 
                                                 scale_limit=0.1, 
                                                 rotate_limit=5,
-                                                p=0.9)
+                                                p=0.9),
                 albumentations.Normalize(mean, std, always_apply=True)
             ])
 
@@ -43,10 +44,10 @@ class BengaliDatasetTrain:
         image = image.reshape(137, 236).astype(float)
         image = Image.fromarray(image).convert("RGB")
         image = self.aug(image=np.array(image))['image']
-        impage = np.transposeimage, (2,0,1)).astype(np.float32)
+        impage = np.transpose(image, (2,0,1)).astype(np.float32)
 
         return {
-            'image' : torch.tensor(image, dtype=torch.float)
+            'image' : torch.tensor(image, dtype=torch.float),
             'grapheme_root': torch.tensor(self.grapheme_root[item], dtype=torch.long),
             'vowel_diacritic': torch.tensor(self.vowel_diacritic[item], dtype=torch.long),
             'consonant_diacritic': torch.tensor(self.consonant_diacritic[item], dtype=torch.long),
