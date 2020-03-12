b4c1a4480afd9379c587fa98270d42b472823319
Deepu Dilip <deepud90@vt.edu>
2020-03-12 16:09:13 +0530

    minor change

--- src/create_image_pickles.py
+++ src/create_image_pickles.py
@@ -13,7 +13,7 @@ def create_image_pickles(parquet_path, output_pickle_path):
         image_array = df.values
         for j, img_id in tqdm(enumerate(image_ids), total=len(image_ids)):
             output_file_name = output_pickle_path + os.sep+ f"{img_id}.pkl"
-            # joblib.dump(image_array[j, :], output_file_name)
+            joblib.dump(image_array[j, :], output_file_name)
 
 
 if __name__ == '__main__':
