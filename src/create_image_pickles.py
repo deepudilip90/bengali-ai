4eb956e9ca079e999de679db86acce7c9d29d34a
Deepu Dilip <deepud90@vt.edu>
2020-03-12 16:01:48 +0530

    minor refactoring

--- src/create_image_pickles.py
+++ src/create_image_pickles.py
@@ -2,13 +2,20 @@ import pandas as pd
 import joblib
 import glob 
 from tqdm import tqdm
+import os
 
-if __name__ == '__main__':
-    files = glob.glob('../input/train_*.parquet')
+def create_image_pickles(parquet_path, output_pickle_path):
+    files = glob.glob(parquet_path)
     for f in files:
         df = pd.read_parquet(f)
         image_ids = df.image_id.values
         df = df.drop('image_id', axis=1)
         image_array = df.values
         for j, img_id in tqdm(enumerate(image_ids), total=len(image_ids)):
-            joblib.dump(image_array[j, :], f"../input/image_pickles/{img_id}.pkl")
+            output_file_name = output_pickle_path + os.sep+ f"{img_id}.pkl"
+            # joblib.dump(image_array[j, :], output_file_name)
+
+
+if __name__ == '__main__':
+    
+    create_image_pickles('../input/train_*.parquet', '../input/image_pickles')
