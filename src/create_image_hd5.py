eef458c9413345be24ab4b575da4aebd6a4c4ae4
Deepu Dilip <deepud90@vt.edu>
2020-03-16 20:46:26 +0530

    script to create hd5 file for images

---
+++ src/create_image_hd5.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import os
+from tqdm import tqdm
+import h5py
+import numpy as np
+from glob import glob
+
+
+## Read parquet images and store to hdf5
+parquet_dir = '/Users/deepudilip/ML/Kaggle /benagli-ai/input/'
+hd5_file_path = '/Users/deepudilip/ML/Kaggle /benagli-ai/input/'
+
+parquet_files = glob(parquet_dir + 'train_*.parquet')
+parquet_files.sort()
+
+hd5file = h5py.File(hd5_file_path + 'image_dataset.h5", "w")
+
+for file in parquet_files:
+    print('Now Reading from file: ', file)
+    df_parquet = pd.read_parquet(file)
+    image_ids = df_parquet.image_id
+    image_arrays = df_parquet.drop('image_id', axis=1).values
+    for idx, image_id in tqdm(enumerate(image_ids), total=len(image_ids)):
+        image = image_arrays[idx,:].reshape(236, 137)
+        hd5file.create_dataset(image_id, np.shape(image), h5py.h5t.STD_U8BE, data=image)
+
+hd5file.close()
+        
+# labels_df = pd.read_csv('/Users/deepudilip/ML/Kaggle /benagli-ai/input/train.csv')
+# label_arrays = labels_df.drop(columns=['image_id', 'grapheme']).values
+# images = np.asarray(images)
+
+        
+#         labels[image_id] = label_arrays
